9806fcb2cb9b78763638acdc21907cf32ec44e3f
bgw <giung609@naver.com>
2020-10-28 09:55:39 +0900

    sample data monotone chain 성공

--- OpenGLstudies/Monotone_Chain/convex.cpp
+++ OpenGLstudies/Monotone_Chain/convex.cpp
@@ -14,37 +14,86 @@ namespace Convex
 	int unit_num = UNIT_NUM;
 
 	// 샘플 좌표
-	float x_array[UNIT_NUM] = { -1.0f, -0.8f, -0.75f, -0.68f, -0.52f, -0.29f, 0.11f, 0.23f, 0.38f, 0.6f };
+	float x_array[UNIT_NUM] = { -0.9f, -0.8f, -0.75f, -0.68f, -0.52f, -0.29f, 0.11f, 0.23f, 0.38f, 0.6f };
 	float y_array[UNIT_NUM] = { 0.0f, 0.8f, -0.3f, 0.2f, 0.1f, -0.4f, 0.25f, 0.51f, -0.15f, 0.0f };
-	bool convex_array_f[UNIT_NUM] = { true };
-	bool convex_array_b[UNIT_NUM] = { true };
+	bool convex_array_f[UNIT_NUM] = { true, true, true, true, true, true, true, true, true, true };
+	bool convex_array_b[UNIT_NUM] = { true, true, true, true, true, true, true, true, true, true };
 
 	// 벡터 회전방향 검사 ( 스택으로 검사해야 함, 0-1-2 검사때 1이 false 뜨면 1 버리고 0-2-3을 검사해야함 )
+	// 1이 처음 true 떠도 2가 false 되면 좀 이상해지는 경우 있음
 	void vector_rotate()
 	{
 		int start = 0;
 		int judge = 1;
-		int end = 2;
+		int next = 2;
+		int end = 3;
 
 		while (end < UNIT_NUM)
 		{
 			float vx1 = x_array[judge] - x_array[start]; float vy1 = y_array[judge] - y_array[start];
-			float vx2 = x_array[end] - x_array[judge]; float vy2 = y_array[end] - y_array[judge];
+			float vx2 = x_array[next] - x_array[judge]; float vy2 = y_array[next] - y_array[judge];
+			float vx3 = x_array[end] - x_array[next]; float vy3 = y_array[end] - y_array[next];
 
-			float cross = vx1 * vy2 - vx2 * vy1;
+			float cross12 = vx1 * vy2 - vx2 * vy1;
+			float cross23 = vx2 * vy3 - vx3 * vy2;
 
-			if (cross >= 0)
+			if (cross12 < 0)
+			{
+				convex_array_f[judge] = false;
+				judge = next;
+				next = end;
+				end = end + 1;
+			}
+			else if (cross23 < 0)
+			{
+				convex_array_f[next] = false;
+				next = end;
+				end = end + 1;
+			}
+			else
 			{
 				convex_array_f[judge] = true;
 				start = judge;
-				judge = end;
+				judge = next;
+				next = end;
 				end = end + 1;
 			}
-			else if (cross < 0)
+		}
+
+		start = UNIT_NUM - 1;
+		judge = UNIT_NUM - 2;
+		next = UNIT_NUM - 3;
+		end = UNIT_NUM - 4;
+
+		while (end >= 0)
+		{
+			float vx1 = x_array[judge] - x_array[start]; float vy1 = y_array[judge] - y_array[start];
+			float vx2 = x_array[next] - x_array[judge]; float vy2 = y_array[next] - y_array[judge];
+			float vx3 = x_array[end] - x_array[next]; float vy3 = y_array[end] - y_array[next];
+
+			float cross12 = vx1 * vy2 - vx2 * vy1;
+			float cross23 = vx2 * vy3 - vx3 * vy2;
+
+			if (cross12 < 0)
 			{
-				convex_array_f[judge] = false;
-				judge = end;
-				end = end + 1;
+				convex_array_b[judge] = false;
+				judge = next;
+				next = end;
+				end = end - 1;
+			}
+			else if (cross23 < 0)
+			{
+				convex_array_b[next] = false;
+				next = end;
+				end = end - 1;
+			}
+			else
+			{
+				convex_array_b[judge] = true;
+				start = judge;
+				judge = next;
+				next = end;
+				end = end - 1;
 			}
 		}
 	}
@@ -66,26 +115,34 @@ namespace Convex
 		// 선
 		glColor3f(0.0f, 1.0f, 0.0f);
 		glBegin(GL_LINE_STRIP);
-
 		for (int i = 0; i < UNIT_NUM; i++)
 		{
 			if (convex_array_f[i])
 			{
+				
 				glVertex2f(x_array[i], y_array[i]);
 			}
-			else
+		}
+		glEnd();
+
+		// 선
+		glColor3f(0.0f, 0.0f, 1.0f);
+		glBegin(GL_LINE_STRIP);
+		for (int i = UNIT_NUM - 1; i >= 0; i--)
+		{
+			if (convex_array_b[i])
 			{
 
+				glVertex2f(x_array[i], y_array[i]);
 			}
 		}
-
 		glEnd();
 	}
 
 	// unit point
 	void unit_point(float x, float y)
 	{
-		glColor3f(0.0f, 0.0f, 1.0f);
+		glColor3f(0.0f, 0.0f, 0.0f);
 
 		// point 생성
 		glPointSize(5.0);
@@ -151,7 +208,7 @@ namespace Convex
 	{
 		glutInitWindowSize(500, 500);
 		glutInitWindowPosition(300, 300);
-		glutCreateWindow("OpenGL Ideal Particles");
+		glutCreateWindow("OpenGL Monotone Chain");
 
 		glutDisplayFunc(display);
 	}
